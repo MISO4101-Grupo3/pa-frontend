af899dbea18a5ecf0b384c947001f0be024411f8
Juan Hernández <jnhernandz@gmail.com>
2018-02-08 06:21:59 -0500

    modified server configuration to support SAP routing

--- server.js
+++ server.js
@@ -5,5 +5,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+// load the single view file (angular will handle the page changes on the front-end)
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
